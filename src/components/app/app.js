4ccace811a037bdff0eceabddecb77923254b264
Alex <sylkoalex@gmail.com>
2023-01-25 21:07:56 +0200

    add search and filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,10 +16,12 @@ class App extends Component{
     constructor(props){
         super(props)
         this.state = {
-            data : [{name:'alex bugatti', salary:1000, increase:false, rise:true, id:nextId('')},
-                    {name:'ihor', salary:10, increase: false, rise:false, id:nextId()},
-                    {name:'mike tyson', salary:6666, increase: false, rise:false, id:nextId()},
+            data : [{name:'Alex Bugatti', salary:1000, increase:false, rise:true, id:nextId('')},
+                    {name:'Volodymyr Zelenskiy', salary:3000, increase: false, rise:false, id:nextId()},
+                    {name:'Mike Tyson', salary:6666, increase: false, rise:false, id:nextId()},
             ],
+            term: "",
+            filter: ''
         }
     }
 
@@ -56,16 +58,48 @@ class App extends Component{
         }))
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (value)=>{
+        this.setState({term: value})
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
     render(){
+        const{data, term, filter} = this.state
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter)
         return (
         <div className="app">
-            <AppInfo total = {this.state.data.length}
-                     increased = {this.state.data.filter(item=>item.increase).length}/>
+            <AppInfo total = {data.length}
+                     increased = {data.filter(item=>item.increase).length}/>
             <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+                <SearchPanel onUpdateSearch = {this.onUpdateSearch}/>
+                <AppFilter onFilterSelect = {this.onFilterSelect}
+                           filter = {filter}/>
                 <Employeeslist 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete = {this.deleteItem}
                     onToggleProp = {this.onToggleProp}/>
             </div>
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,13 +1,31 @@
+import { Component } from 'react/cjs/react.production.min';
 import './search-panel.css';
 
-const SearchPanel = () => {
-    return (
-        <input 
-            type="text"
-            placeholder='search'
-            className='form-control search-input'
-        />
-    )
+class SearchPanel extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            term:''
+        }
+    }
+
+    onUpdateSearch = (e)=> {
+        let term = e.target.value
+        this.setState({term})
+        this.props.onUpdateSearch(term)
+    }
+
+    render(){
+        return (
+            <input 
+                type="text"
+                placeholder='search'
+                className='form-control search-input'
+                value={this.state.term}
+                onChange = {this.onUpdateSearch}
+            />
+        )
+    }
 }
 
 export default SearchPanel;
